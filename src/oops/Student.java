38e1c83f5f0c855dba9f8bdccc16d16538bac3b1
sandeep bhingle <sandeepbhingle@gmail.com>
2022-08-22 09:54:32 +0530

    oops start

---
+++ src/oops/Student.java
@@ -0,0 +1,23 @@
+package oops;
+
+public class Student {
+    // instance variable
+    int rollNumber;
+    String studentName;
+
+    // static variable
+    static String collegeName;
+
+    // instance method
+    void registerStudent(){
+        System.out.println("Student registered ");
+
+    }
+
+    // static method
+    public static void main(String[] args) {
+        int localVariable = 30;
+        System.out.println("Local variable "+localVariable);
+    }
+
+}
